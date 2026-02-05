5d863b60bb3b3ca890e09ed4ea7854377048e537
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-02-06 01:30:22 +0530

    Createdoctor, hospital and patient models

--- src/models/hospital-management/hospital.models.js
+++ src/models/hospital-management/hospital.models.js
@@ -1,5 +1,34 @@
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({}, { timestamps: true });
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      requird: true,
+    },
+    addressLine1: {
+      type: String,
+      required: true,
+    },
+    addressLine2: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required,
+    },
+    pincode: {
+      type: String,
+      required: true,
+    },
+    specializedIn: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true },
+);
 
 export const Hospital = mongoose.model("Hospital", hospitalSchema);
